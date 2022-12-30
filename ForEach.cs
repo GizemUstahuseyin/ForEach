1b13027ec3ab8584e5ba3734148d7112b1ea1eac
Gizem <gizemustahuseyin123@gmail.com>
2022-12-30 19:57:05 +0300

    Add files via upload

---
+++ ForEach.cs
@@ -0,0 +1,15 @@
+﻿namespace ForEach
+{
+    internal class ForEach
+    {
+        static void Main(string[] args)
+        {
+            string[] students = new string[3] { "Gizem", "Mehmet", "Güven" };
+            foreach (var student in students)
+            {
+                Console.WriteLine(student);
+            }
+            Console.ReadLine();
+        }
+    }
+}
